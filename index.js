8b12ab7a2e4d07a8cb51ea515af97ab76f508ef6
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2025-03-12 19:08:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-const express = require("express");
-const cors = require("cors");
+const eliminar = async(id)=>{
+    const response = await fetch(
+        "http://localhost:3000/api/v1/"+id,{
+            method:"DELETE",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            // body:JSON.stringify({saludo:"Hola"})
+        });
+    if (response.status == 204){
+        alert("Producto Eliminado correctamente");
+    }else{
+        if (response.ok){
+            const respuesta = await response.json();
+            alert(respuesta.message || "No se pudo eliminar por una causa desconocida");
+        }else{
+            alert("Ocurrio un problema al comunicarse con el servidor, intenta de nuevo más tarde")
+        }
+    }
+}
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-let productos = [];
-
-app.post("/productos", (req, res) => {
-    const { nombre, precio } = req.body;
-    const producto = { nombre, precio };
-    productos.push(producto);
-    res.status(201).send('Producto registrado exitosamente.');
-});
-
-app.get("/productos", (req, res) => {
-    res.json(productos);
-});
-
-app.post("/total", (req, res) => {
-    const total = productos.reduce((acc, producto) => acc + producto.precio, 0);
-    res.json({ total });
-});
-
-// Manejar solicitudes GET en la misma ruta "/total"
-app.get("/total", (req, res) => {
-    const total = productos.reduce((acc, producto) => acc + producto.precio, 0);
-    res.json({ total });
-});
+document.addEventListener("DOMContentLoaded",()=>{
+    const contenedorCards = document.getElementById("contenedor-cards");
 
 
-app.listen(3000, () => console.log("Servidor creado en el puerto 3000"));
+
+    const obtenerProductosApi= async()=>{
+        const response = await fetch("http://localhost:3000/api/v1");
+        
+        if (response.ok){
+            const productos = await response.json();
+            contenedorCards.innerHTML="";
+            productos.map(producto=>{
+                contenedorCards.innerHTML += `
+                <li class="shadow-lg border border-emerald-200 rounded p-4">
+                   <h3 class="text-lg font-bold text-emerald-950 ">${producto.nombre}</h3>
+                    <p class="text-md font-medium text-emerald-500 my-1.5">$${producto.precio}</p>
+                    <span class="block text-sm text-gray-500">stock - ${producto.stock}</span>
+                    <div class="flex gap-3 mt-6">
+                        <button
+                        class="flex-1 px-3 py-2.5 bg-teal-100 text-teal-700 rounded-md hover:bg-teal-200 flex items-center justify-center gap-2 transition-colors duration-200">
+                            <i class="bi bi-pencil-square"></i>
+                            Editar
+                        </button>
+                        <button onclick="eliminar(${producto.id})"
+                        class="flex-1 px-3 py-2.5 bg-red-100 text-red-700 rounded-md hover:bg-red-200 flex items-center justify-center gap-2 transition-colors duration-200">
+                            <i class="bi bi-trash3"></i>
+                            Eliminar
+                        </button>
+                    </div>
+                </li>
+                `;
+            });
+        }
+    }
+  
+    
+    obtenerProductosApi();
+})
