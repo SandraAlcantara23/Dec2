197ea88a238d14f539d1dda64e9454787ea2058a
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2025-03-16 13:37:07 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const eliminar = async(id)=>{
+var isUpdate = false;
+var id = null;
+
+
+
+const eliminar = async(id,boton)=>{
     const response = await fetch(
         "http://localhost:3000/api/v1/"+id,{
             method:"DELETE",
@@ -9,6 +14,8 @@ const eliminar = async(id)=>{
         });
     if (response.status == 204){
         alert("Producto Eliminado correctamente");
+        const card = boton.closest("li");
+        card.remove();
     }else{
         if (response.ok){
             const respuesta = await response.json();
@@ -19,10 +26,19 @@ const eliminar = async(id)=>{
     }
 }
 
+
+
 document.addEventListener("DOMContentLoaded",()=>{
     const contenedorCards = document.getElementById("contenedor-cards");
+    const formulario = document.getElementById("Formulario");
+    const nombreInput = document.getElementById("NombreProducto");
+    const precioInput = document.getElementById("precioProducto");
+    const stockInput = document.getElementById("stockProducto");
+    const btnLimpiar = document.getElementById("btn-limpiar");
 
-
+    btnLimpiar.addEventListener("click",()=>{
+        formulario.reset();
+    })
 
     const obtenerProductosApi= async()=>{
         const response = await fetch("http://localhost:3000/api/v1");
@@ -42,7 +58,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                             <i class="bi bi-pencil-square"></i>
                             Editar
                         </button>
-                        <button onclick="eliminar(${producto.id})"
+                        <button onclick="eliminar(${producto.id},this)"
                         class="flex-1 px-3 py-2.5 bg-red-100 text-red-700 rounded-md hover:bg-red-200 flex items-center justify-center gap-2 transition-colors duration-200">
                             <i class="bi bi-trash3"></i>
                             Eliminar
@@ -53,7 +69,60 @@ document.addEventListener("DOMContentLoaded",()=>{
             });
         }
     }
-  
     
+    const actualizar = (producto)=>{
+        isUpdate= true;
+        id = producto.id;
+        nombreInput.value = producto.nombre;
+        precioInput.value = producto.precio;
+        stockInput.valu = producto.stock;
+
+    }
+    
+
+    const crearProducto = async (evt)=>{
+        evt.preventDefault();
+        const nombre = nombreInput.value;
+        const precio = precioInput.value;
+        const cantidadProducto = stockInput.value;
+
+        const nuevoProducto = {
+            nombre,
+            precio,
+            stock: cantidadProducto
+        }
+        if (!isUpdate){
+            // Crear
+            const response = await fetch("http://localhost:3000/api/v1/",{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body:JSON.stringify(nuevoProducto)
+            });
+            if (response.status == 201){
+                alert("Producto Creado");
+                formulario.reset();
+                obtenerProductosApi();
+            }else{
+                alert("Ocurrio un problema al agregar el producto");
+            }  
+        }else{
+            //actualizar
+            const response = await fetch(`http://localhost:3000/api/v1/${id}`,{
+                method:"PATCH",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(nuevoProducto)
+            })
+            if (response.ok){
+                formulario.reset();
+                obtenerProductosApi();
+            }
+        }
+    }
+
+    formulario.addEventListener("submit",crearProducto);
     obtenerProductosApi();
 })
